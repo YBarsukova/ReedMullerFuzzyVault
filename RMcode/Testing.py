1612151601490b17b0cc6e88d478d356dc0cc870
YBarsukova <yaroslavabarsukova@internet.ru>
2024-11-01 23:22:36 +0300

    added a file to create statistics on the number of errors that are not guaranteed to be corrected

---
+++ RMcode/Testing.py
@@ -0,0 +1,52 @@
+import itertools
+import math
+
+import numpy as np
+from tqdm import tqdm
+import RM
+
+def generate_error_combinations(message_length, num_errors):
+
+    positions = range(message_length)
+    return list(itertools.combinations(positions, num_errors))
+
+def apply_errors(encoded_message, error_positions):
+
+    corrupted_message = encoded_message.copy()
+    for ind in error_positions:
+        corrupted_message[ind] ^= 1
+    return corrupted_message
+
+def test_error_correction(code, message, num_errors):
+    encoded_message = code.encode(message)
+    message_length = len(encoded_message)
+    error_combinations = generate_error_combinations(message_length, num_errors)
+    total_tests = len(error_combinations)
+    successful_corrections = 0
+
+    for error_positions in tqdm(error_combinations, desc=f"Testing {num_errors} errors"):
+        corrupted_message = apply_errors(encoded_message, error_positions)
+        decoded_message = code.Decode(corrupted_message)
+        if decoded_message == message:
+            successful_corrections += 1
+
+    unsuccessful_corrections = total_tests - successful_corrections
+    return successful_corrections, unsuccessful_corrections
+
+def run_tests_for_error_counts(code, message):
+    max_errors = code.n
+    message_length = len(code.encode(message))
+    max_errors = int(max_errors)
+
+    results = []
+
+    for num_errors in range(1, max_errors):
+        total_combinations = math.comb(message_length, num_errors)
+        if total_combinations > 1e6:
+            print(f"Слишком много комбинаций для {num_errors} ошибок ({total_combinations} комбинаций). Пропуск...")
+            continue
+        successful, unsuccessful = test_error_correction(code, message, num_errors)
+        results.append((num_errors, successful, unsuccessful))
+        print(f"\n Для {num_errors} ошибок: Успешно исправлено {successful} из {successful + unsuccessful}")
+
+    return results
--- RMcode/main.py
+++ RMcode/main.py
@@ -1,23 +1,35 @@
 import time
-import random
 import RM
+import Testing
+
+def generate_deterministic_message(number, length):
+    binary_message = list(map(int, bin(number)[2:].zfill(length)))
+    return binary_message
+
 
 def measure_main_execution_time():
     start_time = time.time()
-    c=RM.RM(4,2)
-    for i in range(200000):
-        message = [random.choice([0,1]) for i in range(RM.sum_binomial(c.m, c.r))]
-        emessage=c.encode(message)
-        #print(emessage)
-        emessage[2]=emessage[2]^1
-        for j in range(3):
-            emessage[j] = 3
-        #print(emessage)
-        d = c.Decode(emessage)
-        #print(d)
-        assert message == d
-    print(c.decode2(emessage))
+    c = RM.RM(8, 3)
+    message_length = RM.sum_binomial(c.m, c.r)
+    message = generate_deterministic_message(42, message_length)
+    max_value = 2 ** message_length
+
+    # for repeat in range(20):
+    #
+    #     for number in range(max_value):
+    #         message = generate_deterministic_message(number, message_length)
+    #         emessage = c.encode(message)
+    #         emessage[2] = emessage[2] ^ 1
+    #         for j in range(3):
+    #             emessage[j] = 3
+    #
+    #         d = c.Decode(emessage)
+    #         assert message == d, f"Decoded message does not match the original message for input {message}."
+    results = Testing.run_tests_for_error_counts(c, message)
+    print(results)
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.4f} seconds")
+
+
 measure_main_execution_time()
